1c6c545d88c8f6875ba2a1949da8c543635c87da
TamaraG13 <36139219+TamaraG13@users.noreply.github.com>
2018-03-03 17:51:11 +0100

    Created the calculator project

--- calculator/src/calculator/Main.java
+++ calculator/src/calculator/Main.java
@@ -6,22 +6,34 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-	try(Scanner sc = new Scanner(System.in)) {
-		System.out.println("Enter the first number");
+		try(Scanner sc = new Scanner(System.in)) {
+			
+			System.out.println("Enter the first number");
+			int a = sc.nextInt();
+
+			System.out.println("Enter the second number");
+			int b = sc.nextInt();
+
+			System.out.println("Now calculating the sum");
+			int c = a + b;
 		
-int a = sc.nextInt();
+			System.out.println("First number is:" + a);	
+			System.out.println("Second number is:" + b);
+			System.out.println("Sum is:" + c); 
+			
+			System.out.println("Enter your first number");
+			int aa = sc.nextInt();
+			
+			System.out.println("Enter the second number");
+			int bb = sc.nextInt();			
+			
+			System.out.println("Now calculating the difference");
+			int cc = aa - bb;
+	
+			System.out.println("First number is:" + aa);	
+			System.out.println("Second number is:" + bb);
+			System.out.println("Sum is:" + cc); 
 
-System.out.println("Enter the second number");
-
-int b = sc.nextInt();
-
-System.out.println("Now calculating the sum");
-
-int c = a + b;
-
-System.out.println("First number is:" + a);
-System.out.println("Second number is:" + b);
-System.out.println("Sum is:" + c); 
-}
+		}
 	}
 }
