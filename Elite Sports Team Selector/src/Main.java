a9ac3c396da9fba197268855930104ad4b687d87
Valentina Cupac <valentina.cupac@optivem.com>
2018-03-16 22:07:34 +0100

    #6 Elite Sports Team Selector - Formatting fixes

--- Elite Sports Team Selector/src/Main.java
+++ Elite Sports Team Selector/src/Main.java
@@ -1,9 +1,9 @@
+import java.util.Scanner;
 
-	import java.util.Scanner;
-	public class Main {
+public class Main {
 
 	public static void main(String[] args) {
-		
+	
 		try(Scanner sc = new Scanner(System.in)) {
 			
 			System.out.println("To be accepted to this team you need to fill out the form");
@@ -17,18 +17,15 @@
 			System.out.println("I have experience: true or false?");
 			boolean experience1 = sc.nextBoolean();
 		
-			System.out.println("How much do you weight?");
+			System.out.println("How much do you weigh?");
 			double weight1 = sc.nextDouble();
 		
-			if  (weight1 <= 60&& experience1 == true && age1 >= 15 && gender1 == 'W') {
+			if (weight1 <= 60 && experience1 == true && age1 >= 15 && gender1 == 'W') {
 				System.out.println("Good! You are accepted!");	
 			} else {
 				System.out.println("Unfortunately, you are not accepted");
 			}
-		
-			}
-
 		}
-
 	}
-
+	
+}
